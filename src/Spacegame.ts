805ac0e34ea39047c33878f506485dd4f4d56d12
linews <xin710572078@gmail.com>
2023-09-07 18:29:54 +0800

    0907 changes

--- src/Spacegame.ts
+++
@@ -1,5 +0,0 @@
-export const scene = new Scene()
-export const camera = new PerspectiveCamera(70, 
-window.innerWidth / window.innerHeight, 0.5,9000)
-// Our three renderer
-let renderer: WebGLRenderer;
